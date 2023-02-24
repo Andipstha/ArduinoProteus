68e70ada34f433deb6828165fa4d6c382ddcbd32
andipstha <sandip.stha120@gmail.com>
2023-02-24 20:10:59 +0545

    Added CarSpeedDetector-Project

---
+++ CarSpeedDetector/Code/CarSpeedDetector.ino
@@ -0,0 +1,69 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+int timer1;
+int timer2;
+
+float Time;
+
+int flag1 = 0;
+int flag2 = 0;
+
+float distance = 5.0;
+float speed;
+
+int ir_s1 = A0;
+int ir_s2 = A1;
+
+int buzzer = 13;
+
+void setup(){
+  pinMode(ir_s1, INPUT);
+  pinMode(ir_s2, INPUT);
+  pinMode(buzzer, OUTPUT);
+  
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" Car Speed ");
+  lcd.setCursor(0,1);
+  lcd.print("  Detector ");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+if(digitalRead (ir_s1) == LOW && flag1==0){timer1 = millis(); flag1=1;}
+
+if(digitalRead (ir_s2) == LOW && flag2==0){timer2 = millis(); flag2=1;}
+
+if (flag1==1 && flag2==1){
+     if(timer1 > timer2){Time = timer1 - timer2;}
+else if(timer2 > timer1){Time = timer2 - timer1;}
+ Time=Time/1000;//convert millisecond to second
+ speed=(distance/Time);//v=d/t
+ speed=speed*3600;//multiply by seconds per hr
+ speed=speed/1000;//division by meters per Km
+}
+
+if(speed==0){ 
+lcd.setCursor(0, 1); 
+if(flag1==0 && flag2==0){lcd.print("No car  detected");}
+                    else{lcd.print("Searching...    ");} 
+}
+else{
+    lcd.clear(); 
+    lcd.setCursor(0, 0); 
+    lcd.print("Speed:");
+    lcd.print(speed,1);
+    lcd.print("Km/Hr  ");
+    lcd.setCursor(0, 1); 
+  if(speed > 50){lcd.print("  Over Speeding  "); digitalWrite(buzzer, HIGH);}
+            else{lcd.print("  Normal Speed   "); }    
+    delay(3000);
+    digitalWrite(buzzer, LOW);
+    speed = 0;
+    flag1 = 0;
+    flag2 = 0;    
+ }
+}
---
+++ Counters/09Counter/CounterCode/CounterCode.ino
@@ -0,0 +1,153 @@
+//The Coding://
+#define a 1
+#define b 2
+#define c 3
+#define d 4
+#define e 5
+#define f 6
+#define g 7
+#define count1 8
+#define count2 9
+
+
+
+void setup() 
+{
+  pinMode(a,OUTPUT); 
+  pinMode(b,OUTPUT); 
+  pinMode(c,OUTPUT); 
+  pinMode(d,OUTPUT); 
+  pinMode(e,OUTPUT); 
+  pinMode(f,OUTPUT); 
+  pinMode(g,OUTPUT);
+  pinMode(count1, OUTPUT);
+  pinMode(count2, OUTPUT);
+}
+
+void loop()
+{
+
+  // for(int i=0;i<=9;i++)
+  // {
+  //   for(int j=0;j<=9;j++)
+  //   {
+  //     digitalWrite(count1,HIGH);
+  //     number(j);
+  //     delay(500);
+  //   }
+  //   digitalWrite(count2,HIGH);
+  //   number(i);
+  //   delay(500);
+  // }
+    //digitalWrite(count1,HIGH);
+  for(int i=0;i<=9;i++)
+  {
+    number(i);
+    delay(500);
+  }
+
+}
+
+void number(int xx)
+{
+  if(xx==0){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,LOW);
+    }
+
+  if(xx==1){
+    digitalWrite(a,LOW);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    digitalWrite(e,LOW);
+    digitalWrite(f,LOW);
+    digitalWrite(g,LOW);
+  }
+
+  if(xx==2){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,LOW);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,LOW);
+    digitalWrite(g,HIGH);
+  }
+
+  if(xx==3){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,LOW);
+    digitalWrite(f,LOW);
+    digitalWrite(g,HIGH);
+  }
+
+  if(xx==4){
+    digitalWrite(a,LOW);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    digitalWrite(e,LOW);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+  }
+
+  if(xx==5){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,LOW);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,LOW);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+  }
+
+  if(xx==6){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,LOW);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+  }
+
+  if(xx==7){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    digitalWrite(e,LOW);
+    digitalWrite(f,LOW);
+    digitalWrite(g,LOW);
+  }
+
+  if(xx==8){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+  }
+
+  if(xx==9){
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,LOW);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+  }
+}
+
